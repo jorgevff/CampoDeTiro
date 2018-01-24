a2c6f415b8367fe8e4975c6ab37b056987f17905
jorgevff <jvalladaresfontano@gmail.com>
2018-01-24 12:26:09 +0100

    Se crea la clase CampoDeTiro con los campos y el metodo añadir

---
+++ CampoDeTiro.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.time.LocalDate;
+/**
+ * Write a description of class CampoDeTiro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CampoDeTiro
+{
+    //se crea un campo para guardar una coleccion de armas registradas
+    private ArrayList<Arma> armas;
+    int numeroIdentificativo;
+    //Se crea el constructor y se incializa nuestro ArrayList
+    public CampoDeTiro()
+    {
+        armas = new ArrayList<Arma>();
+        numeroIdentificativo = 0;
+    }
+    
+    /**
+     * Se añaden 
+     */
+   
+    public void addArma(String modelo, int numeroSerie)
+    {
+       
+        Arma nuevaArma = new Arma(numeroIdentificativo, modelo, numeroSerie);
+        numeroIdentificativo++;
+        armas.add(nuevaArma);
+    }
+}
